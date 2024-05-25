0b77334cc99719c4d6ba03fb75ce2b14d2e79b67
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-25 22:01:20 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -8,24 +8,58 @@
 #include <sstream>
 #include "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-    BST<std::string> tree;
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("Ошибка открытия файла");
+node* add(node* node, const t& value) {
+    if (node == nullptr) {
+        return new node(value);
     }
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    file.close();
-    std::string content = buffer.str();
-    std::regex wordregex("[a-zA-Z]+");
-    std::smatch match;
-    while (std::regex_search(content, match, wordregex)) {
-        std::string word = match.str();
-        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-        tree.insert(word);
-        content = match.suffix();
+    node* current = node;
+    node* parent = nullptr;
+    while (current != nullptr) {
+        parent = current;
+        if (current->data > value) {
+            current = current->left;
+        } else if (current->data < value) {
+            current = current->right;
+        } else {
+            current->count++;
+            return node;
+        }
     }
-    return tree;
+    if (parent->data > value) {
+        parent->left = new node(value);
+    } else {
+        parent->right = new node(value);
+    }
+    return node;
 }
+int depthoftree(node* node) const {
+    if (node == nullptr) {
+        return 0;
+    }
+    return 1 + std::max(depthoftree(node->left), depthoftree(node->right));
+}
+int wordsearch(node* node, const t& value) const {
+    while (node != nullptr) {
+        if (node->data == value) {
+            return node->count;
+        } else if (node->data < value) {
+            node = node->right;
+        } else {
+            node = node->left;
+        }
+    }
+    return 0;
+}
+
+public:
+    bst() : root(nullptr) {}
+    void insert(const t& value) {
+        root = add(root, value);
+    }
+    int depth() const {
+        return depthoftree(root) - 1;
+    }
+    int search(const t& value) const {
+        return wordsearch(root, value);
+    }
 #endif  // INCLUDE_BST_H_
