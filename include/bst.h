05939cf5ae25bedaf3dc46d2d656aa3033a59fb0
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-25 23:19:42 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,82 +1,103 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-#include <algorithm>
+#include <iostream>
+#include <fstream>
 #include <string>
-#include <sstream>
-#include "bst.h"
-template <typename T>
 
-    class BST {
-        public:
-BST() {
+template <typename T>
+class bst {
+public:
+    bst() {
         root = nullptr;
-}
-     struct Node {
+    }
+    struct node {
         T data;
         int count;
-        Node* left;
-        Node* right;
-        Node(const T& data) {
-            data = data;
+        node* left;
+        node* right;
+        node(const T& data) {
+            this->data = data;
             count = 1;
             left = nullptr;
             right = nullptr;
         }
-                  };
-    void insert(const t& value) {
-        root = add(root, value);
-    }
-    int depth() const {
-        return depthoftree(root) - 1;
-    }
-    int search(const t& value) const {
-        return wordsearch(root, value);
-    }
+    };
+    void insert(const T& data);
+    void inorderTraversal() const;
+    int size() const;
+    ~bst();
 
- private:
-node* add(node* node, const t& value) {
-    if (node == nullptr) {
-        return new node(value);
+private:
+    node* root;
+    void insertNode(node*& node, const T& data);
+    void inorderTraversalNode(node* node) const;
+    int sizeNode(node* node) const;
+};
+
+bst<string> makeTree(const char* filename);
+
+template <typename T>
+bst<T>::~bst() {
+    if (root) {
+        deleteTree(root);
     }
-    node* current = node;
-    node* parent = nullptr;
-    while (current != nullptr) {
-        parent = current;
-        if (current->data > value) {
-            current = current->left;
-        } else if (current->data < value) {
-            current = current->right;
-        } else {
-            current->count++;
-            return node;
-        }
-    }
-    if (parent->data > value) {
-        parent->left = new node(value);
-    } else {
-        parent->right = new node(value);
-    }
-    return node;
 }
-int depthoftree(node* node) const {
+
+template <typename T>
+void bst<T>::insert(const T& data) {
+    insertNode(root, data);
+}
+
+template <typename T>
+void bst<T>::insertNode(node*& node, const T& data) {
+    if (node == nullptr) {
+        node = new node(data);
+        return;
+    }
+    if (data < node->data) {
+        insertNode(node->left, data);
+    } else if (data > node->data) {
+        insertNode(node->right, data);
+    } else {
+        node->count++;
+    }
+}
+
+template <typename T>
+void bst<T>::inorderTraversal() const {
+    inorderTraversalNode(root);
+}
+
+template <typename T>
+void bst<T>::inorderTraversalNode(node* node) const {
+    if (node != nullptr) {
+        inorderTraversalNode(node->left);
+        cout << node->data << " : " << node->count << endl;
+        inorderTraversalNode(node->right);
+    }
+}
+
+template <typename T>
+int bst<T>::size() const {
+    return sizeNode(root);
+}
+
+template <typename T>
+int bst<T>::sizeNode(node* node) const {
     if (node == nullptr) {
         return 0;
     }
-    return 1 + std::max(depthoftree(node->left), depthoftree(node->right));
+    return 1 + sizeNode(node->left) + sizeNode(node->right);
 }
-int wordsearch(node* node, const t& value) const {
-    while (node != nullptr) {
-        if (node->data == value) {
-            return node->count;
-        } else if (node->data < value) {
-            node = node->right;
-        } else {
-            node = node->left;
-        }
+
+bst<string> makeTree(const char* filename) {
+    bst<string> tree;
+    ifstream file(filename);
+    string value;
+    while (file >> value) {
+        tree.insert(value);
     }
-    return 0;
+    file.close();
+    return tree;
 }
-};
 
 #endif  // INCLUDE_BST_H_
