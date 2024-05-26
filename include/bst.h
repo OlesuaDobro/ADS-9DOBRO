1722fc474cfbbd5aebc042a82d3737668f1db954
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-26 22:46:04 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,92 +3,63 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-template <typename T>
-class BST {
- public:
-    BST() {
-        root = nullptr;
-    }
-    struct node {
-        T data;
-        int count;
-        node* left;
-        node* right;
-        node(const T& data) {
-            this->data = data;
-            count = 1;
-            left = nullptr;
-            right = nullptr;
-        }
-    };
-    void insert(const T& data);
-    void inorderTraversal() const;
-    int size() const;
-    ~BST();
- private:
-    node* root;
-    void insertNode(node*& node, const T& data);
-    void inorderTraversalNode(node*& node) const;
-    int sizeNode(node* node) const;
+template <typename T> 
+class BST { 
+private: 
+    struct Node { 
+        T data; 
+        int count; 
+        Node* left; 
+        Node* right; 
+    }; 
+    Node* root; 
+    int depthThree(Node* root) { 
+        if (root == nullptr) { 
+            return 0; 
+        } 
+        return std::max(depthThree(root->left), depthThree(root->right)) + 1; 
+    } 
+    Node* addN(Node* root, const T& data) { 
+        if (root == nullptr) { 
+            root = new Node; 
+            root->data = data; 
+            root->count = 1; 
+            root->left = root->right = nullptr; 
+        } else if (root->data > data) { 
+            root->left = addN(root->left, data); 
+        } else if (root->data < data) { 
+            root->right = addN(root->right, data); 
+        } else { 
+            root->count += 1; 
+        } 
+        return root; 
+    } 
+ 
+    Node* searchThree(Node* root, const T& data) { 
+        if (root == nullptr || root->data == data) { 
+            return root; 
+        } 
+        if (data < root->data) { 
+            return searchThree(root->left, data); 
+        } 
+        return searchThree(root->right, data); 
+    } 
+public: 
+    BST() : root(nullptr) {} 
+    void add(const T& data) { 
+        root = addN(root, data); 
+    } 
+    int depth() { 
+        return depthThree(root) - 1; 
+    } 
+    int search(const T& data) { 
+        Node* curr = searchThree(root, data); 
+        if (curr == nullptr) { 
+            return 0; 
+        } 
+        else { 
+            return curr->count; 
+        } 
+    } 
 };
-
-BST<string> makeTree(const char* filename);
-template <typename T>
-BST<T>::~BST() {
-    if (root) {
-        deleteTree(root);
-    }
-}
-template <typename T>
-void BST<T>::insert(const T& data) {
-    insertNode(root, data);
-}
-template <typename T>
-void BST<T>::insertNode(node*& node, const T& data) {
-    if (node == nullptr) {
-        node = new node(data);
-        return;
-    }
-    if (data < node->data) {
-        insertNode(node->left, data);
-    } else if (data > node->data) {
-        insertNode(node->right, data);
-    } else {
-        node->count++;
-    }
-}
-template <typename T>
-void BST<T>::inorderTraversal() const {
-    inorderTraversalNode(root);
-}
-template <typename T>
-void BST<T>::inorderTraversalNode(node* node) const {
-    if (node != nullptr) {
-        inorderTraversalNode(node->left);
-        cout << node->data << " : " << node->count << endl;
-        inorderTraversalNode(node->right);
-    }
-}
-template <typename T>
-int BST<T>::size() const {
-    return sizeNode(root);
-}
-template <typename T>
-int BST<T>::sizeNode(node* node) const {
-    if (node == nullptr) {
-        return 0;
-    }
-    return 1 + sizeNode(node->left) + sizeNode(node->right);
-}
-BST<string> makeTree(const char* filename) {
-    BST<string> tree;
-    ifstream file(filename);
-    string value;
-    while (file >> value) {
-        tree.insert(value);
-    }
-    file.close();
-    return tree;
-}
-
 #endif  // INCLUDE_BST_H_
