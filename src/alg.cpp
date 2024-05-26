7d1e35fc4a892b66ee938a3d9c2a3eec8c0da7de
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-05-26 22:47:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,28 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-#include <algorithm>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include "bst.h"
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  <string>
+#include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
     BST<std::string> tree;
     std::ifstream file(filename);
     if (!file.is_open()) {
-        throw std::runtime_error("Ошибка открытия файла");
+        std::cout << "Ошибка открытия файла" << std::endl;
+        return tree;
     }
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    file.close();
-    std::string content = buffer.str();
-    std::regex wordregex("[a-zA-Z]+");
-    std::smatch match;
-    while (std::regex_search(content, match, wordregex)) {
-        std::string word = match.str();
-        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-        tree.insert(word);
-        content = match.suffix();
+    std::string word;
+    while (file) {
+        char str = file.get();
+        if (isalpha(str)) {
+            str = tolower(str);
+            word += str;
+        } else if (!(word.empty())) {
+            tree.insert(word);
+            word = "";
+        }
     }
     return tree;
 }
-#endif  // INCLUDE_BST_H_
